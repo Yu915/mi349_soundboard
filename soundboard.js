eedc86d3e7e39e5c260a4ac801747350c9c46a9f
Yu915 <yutianha@msu.edu>
2020-11-15 21:32:22 +0800

    New mi349 files

---
+++ soundboard.js
@@ -0,0 +1,17 @@
+var playCannon = document.getElementById("c1");
+
+var playJet= document.getElementById("j1");
+
+var playStorm = document.getElementById("s1");
+
+function Cannon() {
+    playCannon.play();
+}
+
+function Jet() {
+    playJet.play();
+}
+
+function Storm() {
+    playStorm.play();
+}
